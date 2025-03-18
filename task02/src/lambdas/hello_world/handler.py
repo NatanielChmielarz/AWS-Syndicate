7154db055d42c3c2b578aaa41168ae31d3547c16
Nataniel Chmielarz <NatanielChmielarz1@gmail.com>
2025-03-18 12:01:21 +0100

    update handler

--- task02/src/lambdas/hello_world/handler.py
+++ task02/src/lambdas/hello_world/handler.py
@@ -2,21 +2,8 @@ from commons.log_helper import get_logger
 from commons.abstract_lambda import AbstractLambda
 import json
 
-_LOG = get_logger('LambdaHandler')
 
 class HelloWorldHandler(AbstractLambda):
-
-    def check_request_validity(self, event) -> dict:
-        """
-        Validate if the required fields are present in the event.
-        """
-        if not event.get('requestContext', {}).get('http'):
-            return {"error": "Request context or HTTP method missing."}
-
-        if not all(key in event['requestContext']['http'] for key in ['method', 'path']):
-            return {"error": "Missing HTTP method or path in event."}
-
-        return {}
 
     def process_request(self, event, context):
         """
@@ -24,44 +11,21 @@ class HelloWorldHandler(AbstractLambda):
         """
         method = event.get('requestContext', {}).get('http', {}).get('method')
         path = event.get('requestContext', {}).get('http', {}).get('path')
-        _LOG.info(f"Received request: {method} {path}")
 
         if path == '/hello' and method == 'GET':
-            _LOG.info("Request matched the /hello endpoint")
             return {
                 "statusCode": 200,
                 "body": json.dumps({"statusCode": 200,"message": "Hello from Lambda"})
             }
         else:
-            _LOG.error("Unrecognized endpoint or method")
             return {
                 "statusCode": 400,
                 "body": json.dumps({"statusCode": 400,
-                    "message": f"Bad request: Method {method} and path {path} not supported."
+                    "message": f"Bad request syntax or unsupported method. Request path: {path}. HTTP method: {method}"
                 })
             }
 
 HANDLER = HelloWorldHandler()
 
 def lambda_handler(event, context):
-    """
-    The entry point of the Lambda function.
-
-    Args:
-        event: The input event from the trigger.
-        context: The Lambda execution context.
-
-    Returns:
-        The response from the handler.
-    """
-    _LOG.debug(f"Received event: {event}")
-    
-    errors = HANDLER.check_request_validity(event)
-    if errors:
-        _LOG.error(f"Validation failed: {errors['error']}")
-        return {
-            "statusCode": 400,
-            "body": errors.get("error", "Invalid request format.")
-        }
-
     return HANDLER.process_request(event, context)
