dd3dcdbc2946020d479180d0ac6782ae096eccc0
Nataniel Chmielarz <NatanielChmielarz1@gmail.com>
2025-03-20 10:20:38 +0100

    task03 implementation

---
+++ task03/src/lambdas/hello_world/handler.py
@@ -0,0 +1,37 @@
+from commons.log_helper import get_logger
+from commons.abstract_lambda import AbstractLambda
+import json
+_LOG = get_logger(__name__)
+
+
+class HelloWorld(AbstractLambda):
+
+    def validate_request(self, event) -> dict:
+        if event.get('path') == '/hello':
+            return {
+                "statusCode": 200,
+                "body": json.dumps({"message": "Hello from Lambda"})
+            }
+        else:
+            return None
+        
+    def handle_request(self, event, context):
+        """
+        Explain incoming event here
+        """
+        # todo implement business logic
+        result =  {
+         "statusCode": 200,
+         "message": "Hello from Lambda"
+        }
+        return result
+    
+
+HANDLER = HelloWorld()
+
+
+def lambda_handler(event, context):
+    response = HANDLER.validate_request(event)
+    if response:
+        return response
+    return HANDLER.lambda_handler(event=event, context=context)
